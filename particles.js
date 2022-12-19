25c09f3d2d9d1d1b98018ed27f2d5088638a8052
Riwane <108633987+Riwanskii@users.noreply.github.com>
2022-12-19 09:34:57 +0100

    Create particles.js

---
+++ particles.js
@@ -0,0 +1,15 @@
+.hymne {
+  display: inline-block;
+  margin: 0 0.5rem;
+
+  animation: fadeInLeft; /* referring directly to the animation's @keyframe declaration */
+  animation-duration: 2s; /* don't forget to set a duration! */
+}
+.collab {
+  display: inline-block;
+  margin: 0 0.5rem;
+
+  animation: fadeInRight; /* referring directly to the animation's @keyframe declaration */
+  animation-duration: 2s; /* don't forget to set a duration! */
+}
+
